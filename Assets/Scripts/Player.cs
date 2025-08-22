42486cd0daa1c19621298e250469c8e90e461747
Fernando Posser Pinheiro <17529@atitus.local>
2025-08-22 20:26:42 -0300

    implementei velocidade horizontal

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb;
+
+    [SerializeField] float horizontalSpeed;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float x = Input.GetAxis("Horizontal") * horizontalSpeed;
+        rb.linearVelocity = new Vector2(x, rb.linearVelocityY);
     }
 }
