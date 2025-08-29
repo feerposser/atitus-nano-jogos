aed185cf2942733c7bbebb19b8584fdfcc9ca166
Fernando Posser Pinheiro <17529@atitus.local>
2025-08-28 22:12:46 -0300

    Animações do player implementadas | inimigo patrulheiro implementado

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,10 +2,17 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    [SerializeField] Transform player;
-    [SerializeField] float speed;
-
     Rigidbody2D rb;
+    private Vector2 _direction = Vector2.right;
+
+    [Header("Movimentação")]
+    [SerializeField] private float _speed;
+
+    [Header("Comportamento")]
+    [SerializeField] private LayerMask _groundLayer;
+    [SerializeField] private float _wallDistance;
+    [SerializeField] private float _groundDistance;
+    [SerializeField] private Vector2 _positionOffSet;
 
     private void Start()
     {
@@ -14,8 +21,31 @@ public class Enemy : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // persiga o player
-        Vector2 direction = (player.position - transform.position).normalized;
-        rb.linearVelocity = direction * speed;
+        rb.linearVelocity = new Vector2(_direction.x * _speed, 0);
+
+        if (!HasFrontGround() || HasFrontWall())
+        {
+            _speed *= -1;
+            _positionOffSet = new Vector2(_positionOffSet.x * -1, _positionOffSet.y);
+        }
     }
+
+    private bool HasFrontGround()
+    {
+        return Physics2D.Raycast((Vector2)transform.position + _positionOffSet, Vector2.down, _groundDistance, _groundLayer);
+    }
+
+    private bool HasFrontWall()
+    {
+        return Physics2D.Raycast((Vector2)transform.position + _positionOffSet, _direction, _wallDistance, _groundLayer);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Vector2 origin = (Vector2)transform.position + _positionOffSet;
+        Vector2 to = origin + (Vector2.down * _groundDistance);
+        Gizmos.DrawLine(origin, to);
+    }
+
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+using UnityEngine.InputSystem;
+using UnityEngine.Rendering;
+
 public class Player : MonoBehaviour
 {
     Rigidbody2D rb;
@@ -21,10 +24,18 @@ public class Player : MonoBehaviour
     void Update()
     {
         anim.SetFloat("xSpeed", Mathf.Abs(x));
+        anim.SetFloat("ySpeed", rb.linearVelocityY);
         anim.SetBool("isGrounded", isGrounded);
 
         x = Input.GetAxis("Horizontal") * horizontalSpeed;
-        
+
+        if (x > 0)
+            transform.eulerAngles = new Vector3(0, 0, 0);
+        else if (x < 0)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+
+
+
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             rb.AddForce(Vector2.up * verticalSpeed, ForceMode2D.Impulse);
